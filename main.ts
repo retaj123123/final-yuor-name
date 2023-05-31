383f5971a6de1a4e578099208904c25835af73b5
retaj123123 <135163859+retaj123123@users.noreply.github.com>
2023-05-31 22:27:17 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-default on_button_bressed_a():
-        basic.show_icon(IconNames.Heart)
+def on_button_bressed_a():
+    basic.show_icon(IconNames.Heart)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_gesture_shake():
